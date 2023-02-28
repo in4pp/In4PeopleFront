3f4d8458c6a702172c3aec090b4894599e888efc
blaqonly <blaqonly@gmail.com>
2023-02-28 12:02:36 +0900

    workset commit

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,33 +3,32 @@ import Navbar from "../components/common/Navbar";
 import Sidebar from "../components/common/Sidebar";
 import LayoutCSS from "../components/common/Navbar.module.css";
 function Layout() {
+  return (
+    <>
+      <div style={{ display: "block" }} className={LayoutCSS.mSgFN}>
+        <Navbar />
 
-    return (
-        <>
-            <div style={{ display: 'block' }} className={LayoutCSS.mSgFN}>
-                <Navbar />
+        <Sidebar />
 
-                <Sidebar />
+        <div></div>
+        <div>
+          <div className={LayoutCSS.wrap}>
+            <div></div>
+            <div className={LayoutCSS.wrapper}>
+              <div className={LayoutCSS.ct}>
+                <div className={LayoutCSS.container2}>
+                  {/* content */}
+                  {/* <Outlet /> */}
 
-                <div></div>
-                <div>
-                    <div className={LayoutCSS.wrap}>
-                        <div></div>
-                        <div className={LayoutCSS.wrapper} >
-                            <div className={LayoutCSS.ct}>
-                                <div className={LayoutCSS.container2}>
-                                    {/* content */}
-                                    {/* <Outlet /> */}
-
-                                    <h1>안녕!</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  <h1>안녕!</h1>
                 </div>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Layout;
