8450e2642f778ab12ff07bcfbc50f3dba914ba8a
minjae <minjae>
2023-02-28 14:52:34 +0900

    병합완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import ApprovalSidebar from './sidebars/ApprovalSidebar';
 import TaskSubmit from './pages/task/TaskSubmit'
+import IncomeTax from './pages/salary/IncomeTax';
+import SalarySidebar from './sidebars/SalarySidebar';
+import SalaryInfo from './pages/salary/SalaryInformation';
+
+
 
 
 function App() {
@@ -14,6 +19,15 @@ function App() {
           <Route path="/taskApproval" element={<ApprovalSidebar />}>
             <Route index element={<TaskSubmit />}/>
           </Route>
+          
+          <Route path="/salary" element={<SalarySidebar />}>
+            <Route index element={<SalaryInfo />}/>
+          </Route>
+
+          <Route path="/incomeTax" element={<SalarySidebar />}>
+            <Route index element={<IncomeTax />}/>
+          </Route>
+          
 
         </Route>
 
