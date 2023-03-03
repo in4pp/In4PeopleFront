54ae57b57903aceda4830bd2e018b1e9c46613fc
minjae <minjae>
2023-03-03 17:56:13 +0900

    03-03병합완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
-import ApprovalSidebar from './sidebars/ApprovalSidebar';
-import TaskSubmit from './pages/task/TaskSubmit'
+// import ApprovalSidebar from './sidebars/ApprovalSidebar';
+// import TaskSubmit from './pages/task/TaskSubmit'
 import IncomeTax from './pages/salary/IncomeTax';
 import SalarySidebar from './sidebars/SalarySidebar';
 import SalaryInfo from './pages/salary/SalaryInformation';
@@ -12,34 +11,16 @@ import WorkInquiry from './pages/salary/WorkInquiry';
 
 
 
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout";
-import IncomeTax from "./pages/salary/IncomeTax";
-import SalarySidebar from "./sidebars/SalarySidebar";
-import SalaryInfo from "./pages/salary/SalaryInformation";
-import TaskSidebar from "./pages/task/TaskSidebar";
-import ApprovalSubmit from "./pages/task/taskApproval/ApprovalSubmit";
-import Main from "./pages/main/Main";
-import ApprovalBookmark from "./pages/task/taskApproval/ApprovalBookmark";
-import WorkManageSidebar from "./pages/workManagement/WorkManageSidebar";
-import PersonnelSidebar from "./pages/personnel/PersonnelSidebar";
->>>>>>> origin/master
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="task" element={<TaskSidebar />}>
-            <Route index element={<ApprovalSubmit />} />
-            <Route path="bookMark" element={<ApprovalBookmark />} />
-            <Route path="approval" element={<ApprovalSubmit />} />
-          </Route>
-
           <Route path="/salary" element={<SalarySidebar />}>
             <Route index element={<SalaryInfo />} />
+            
           </Route>
 
           <Route path="/incomeTax" element={<SalarySidebar />}>
@@ -58,10 +39,11 @@ function App() {
           <Route path="/workInquiry" element={<SalarySidebar />}>
             <Route index element={<WorkInquiry />}/>
           </Route>
-
-          <Route path="/workManage" element={<WorkManageSidebar />}></Route>
-
+        
         </Route>
+          
+
+     
       </Routes>
     </BrowserRouter>
   );
