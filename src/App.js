2f09d6395ae4055de28921c428c49c2fd5a2839e
blaqonly <blaqonly@gmail.com>
2023-03-03 11:50:20 +0900

    side

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import ApprovalSidebar from "./sidebars/ApprovalSidebar";
 import TaskSubmit from "./pages/task/TaskSubmit";
-import WorkManageSidebar from "./pages/workManagement/components/WorkManageSidebar";
+import WorkManageSidebar from "./pages/workManagement/WorkManageSidebar";
 
 function App() {
   return (
