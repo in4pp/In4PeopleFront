95e11b58533daecdb0ff3e07912d693e8f2b6d10
hyeeun47113 <hyeeun47113@gmail.com>
2023-03-10 19:21:50 +0900

    인사발령 커밋

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './Store';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './Store';
+
 
 
 
