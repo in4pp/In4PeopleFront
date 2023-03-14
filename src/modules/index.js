e4bdc8ce7599f851677fceb3ac023589a0b48c68
blaqonly <blaqonly@gmail.com>
2023-03-14 23:28:45 +0900

    근무설정불러오기

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
-import memberReducer from './MemberModule'
-import orderInfoReducer from '../modules/PersonnelModule';
+import memberReducer from "./MemberModule";
+import orderInfoReducer from "../modules/PersonnelModule";
 import approvalReducer from "./ApprovalModule";
+import workmanageReducer from "./WorkmanageModule";
 
 const rootReducer = combineReducers({
-    memberReducer,
-    orderInfoReducer,
-    approvalReducer
+  memberReducer,
+  orderInfoReducer,
+  approvalReducer,
+  workmanageReducer,
 });
 
 export default rootReducer;
