bfd907f6007cadf298b8b75f8f6dc65dd59c9ce9
minjae <minjae>
2023-03-16 11:35:12 +0900

    HourlyWage수정

--- src/pages/salary/HourlyWageSetting.js
+++ src/pages/salary/HourlyWageSetting.js
@@ -16,16 +16,30 @@ function HourlyWageSetting() {
               <div className={HourlySettingStyle['jdTeyH']}>
                 <div>
                   <div className={HourlySettingStyle['eieTGn']}>
-                    <h4>포괄임금 설정</h4>
-                    <div className={HourlySettingStyle["dMtzEV"]}>
-                      <div className={HourlySettingStyle["eZeAcE"]}>
-                        <p><button className={HourlySettingStyle['akSTg']}><svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M19 12C19 15.866 15.866 19 12 19C8.13401 19 5 15.866 5 12C5 8.13401 8.13401 5 12 5C15.866 5 19 8.13401 19 12Z" fill="#3366FF" />
-                              <path fillRule="evenodd" clipRule="evenodd" d="M12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75ZM12 21.25C6.89137 21.25 2.75 17.1086 2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25Z" fill="#3366FF" />
-                            </svg></button>포괄임금</p>
-                        <p><button className={HourlySettingStyle['akSTg']}><svg width={22} height={22} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path fillRule="evenodd" clipRule="evenodd" d="M21.75 11C21.75 16.9371 16.9371 21.75 11 21.75C5.06294 21.75 0.25 16.9371 0.25 11C0.25 5.06294 5.06294 0.25 11 0.25C16.9371 0.25 21.75 5.06294 21.75 11ZM1.75 11C1.75 16.1086 5.89137 20.25 11 20.25C16.1086 20.25 20.25 16.1086 20.25 11C20.25 5.89137 16.1086 1.75 11 1.75C5.89137 1.75 1.75 5.89137 1.75 11Z" fill="#CCCCCC" />
-                            </svg></button>비포괄임금</p>
+                  <div className={HourlySettingStyle['fcrGkJ']}>
+                      <h4>은행</h4>
+                      <div className={HourlySettingStyle['emfnGG']}>
+                        
+                              <div className={HourlySettingStyle['jpUIyz']}><select width="480" className={HourlySettingStyle['kSYRJd']}>
+                                            <option value="">은행선택</option>
+                                            <option value="1">국민은행</option>
+                                            <option value="6">우리은행</option>
+                                            <option value="7">기업은행</option>
+                                            <option value="8">농협</option>                           
+                                    </select><svg width="10" height="7" viewBox="0 0 10 7" fill="#000"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.99995 0.25C9.62823 0.25 9.97804 0.976242 9.5864 1.46752L5.6004 6.46752C5.30079 6.84334 4.72998 6.84431 4.42909 6.46952L0.415093 1.46952C0.0210263 0.978653 0.370471 0.25 0.999945 0.25H8.99995Z"
+                                                  fill="current"></path>
+                                    </svg></div>
+                      </div>
+                </div>
+                
+                  </div>
+                  <div className={HourlySettingStyle['eieTGn']}>
+                    <h4>계좌번호</h4>
+                    <div className={HourlySettingStyle['knYfag']}>
+                      <div width={262} className={HourlySettingStyle['kfdWKD']}>
+                        <div className={HourlySettingStyle['kGCoAu']}></div><input className={HourlySettingStyle['dzyuNL']} defaultValue />
                       </div>
                     </div>
                   </div>
